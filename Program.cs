80cb0c75854f4fadd94ff1094cc88787535575e1
Mary Christine Anne Habla <marychristineanne.habla@biotechfarms.net>
2024-01-19 14:38:34 +0800

    changes in calculator app

--- Program.cs
+++ Program.cs
@@ -1,40 +1,38 @@
-﻿// Declare variables and then initialize to zero.
-int num1 = 0; int num2 = 0;
+﻿int integer1 = 0; int integer2 = 0;
 
-// Display title as the C# console calculator app.
-Console.WriteLine("Console Calculator in C#\r");
+Console.WriteLine("Simple Calculator in C#\r");
 Console.WriteLine("------------------------\n");
 
 // Ask the user to type the first number.
 Console.WriteLine("Type a number, and then press Enter");
-num1 = Convert.ToInt32(Console.ReadLine());
+integer1 = Convert.ToInt32(Console.ReadLine());
 
 // Ask the user to type the second number.
 Console.WriteLine("Type another number, and then press Enter");
-num2 = Convert.ToInt32(Console.ReadLine());
+integer2 = Convert.ToInt32(Console.ReadLine());
 
 // Ask the user to choose an option.
 Console.WriteLine("Choose an option from the following list:");
-Console.WriteLine("\ta - Add");
-Console.WriteLine("\ts - Subtract");
-Console.WriteLine("\tm - Multiply");
-Console.WriteLine("\td - Divide");
+Console.WriteLine("\ta - Addition");
+Console.WriteLine("\ts - Subtraction");
+Console.WriteLine("\tm - Multiplication");
+Console.WriteLine("\td - Division");
 Console.Write("Your option? ");
 
 // Use a switch statement to do the math.
 switch (Console.ReadLine())
 {
     case "a":
-        Console.WriteLine($"Your result: {num1} + {num2} = " + (num1 + num2));
+        Console.WriteLine($"Your result: {integer1} + {integer2} = " + (integer1 + integer2));
         break;
     case "s":
-        Console.WriteLine($"Your result: {num1} - {num2} = " + (num1 - num2));
+        Console.WriteLine($"Your result: {integer1} - {integer2} = " + (integer1 - integer2));
         break;
     case "m":
-        Console.WriteLine($"Your result: {num1} * {num2} = " + (num1 * num2));
+        Console.WriteLine($"Your result: {integer1} * {integer2} = " + (integer1 * integer2));
         break;
     case "d":
-        Console.WriteLine($"Your result: {num1} / {num2} = " + (num1 / num2));
+        Console.WriteLine($"Your result: {integer1} / {integer2} = " + (integer1 / integer2));
         break;
 }
 // Wait for the user to respond before closing.
